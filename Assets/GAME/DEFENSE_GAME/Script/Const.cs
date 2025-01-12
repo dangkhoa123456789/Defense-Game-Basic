7be0685c05c31f52085e6b4b0e15cd41868b2c6a
dangkhoa123456789 <devel.dk96@gmail.com>
2025-01-12 21:53:25 +0700

    make script player animation
    write script player

---
+++ Assets/GAME/DEFENSE_GAME/Script/Const.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Const : MonoBehaviour
+{
+    public const string PLAYER_TAG = "Player";
+    public const string ENEMY_TAG = "enemy";
+    public const string ENEMY_WEAPON_TAG = "enemyWeapon";
+
+    public const string ATTACK_ANIM = "Attacking";
+    public const string DEAD_ANIM = "Dead";
+
+    public const string DEAD_PLAYER = "Dead";
+
+    public const string BEST_SCORE_PREF = "Best_score";
+    public const string PLAYER_PREFIX_PREF = "player_";
+    public const string CUR_PLAYER_ID_PREF = "cur_player_id";
+    public const string COIN_PREF = "coin";
+    public const string MUSIC_VOL_PREF = "music_vol";
+    public const string SOUND_VOL_PREF = "sound_vol";
+}
--- Assets/GAME/DEFENSE_GAME/Script/player.cs
+++ Assets/GAME/DEFENSE_GAME/Script/player.cs
@@ -1,35 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class player : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    
+    public float attackRate;
+    private float m_curattackRate;
+    private Animator m_anim;
+    private bool m_isAttacked;
+    private void Awake()
+    {
+        m_anim = GetComponent<Animator>();
+        m_curattackRate = attackRate;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0) && !m_isAttacked)
+        {
+            if (m_anim)
+                m_anim.SetBool(Const.ATTACK_ANIM, true);
+            m_isAttacked = true;
+        }
+        if(m_isAttacked)
+        {
+            m_curattackRate -= Time.deltaTime;
+            if (m_curattackRate <= 0)
+            {
+                m_isAttacked=false;
+                m_curattackRate = attackRate;
+            }
+        }
     }
-    /*private void OnCollisionEnter2D(Collision2D collision)
-     {
-         Debug.Log("da v cham");
-     }*/
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void ResetAttackAnim()
     {
-        
+        if (m_anim)
+            m_anim.SetBool(Const.ATTACK_ANIM, false);
     }
-    private void OnTriggerStay2D(Collider2D collision)
-    {
         
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        
-    }
 }
